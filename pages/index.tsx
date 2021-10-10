d31a11cf74a0044000145641a258c5aff9546bd8
Daniel Wildman <iWildman1@hotmail.com>
2021-10-10 16:07:47 +0100

    github integrated IDE test

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <p>This is a test of the web editor to see how good it is!</p>
         </h1>
 
         <p className={styles.description}>
